963aa44044464fdce33b9fa84d1242b5f4fb7e9f
Yashaswini S <156496246+yasH04-s@users.noreply.github.com>
2024-12-12 20:17:13 +0530

    Update code.py

--- Experiment_1/V2_KeyboardInput/code.py
+++ Experiment_1/V2_KeyboardInput/code.py
@@ -1,1 +1,13 @@
-
+# to get user input for coefficients
+a = float(input("Enter the coefficient a: "))
+b = float(input("Enter the coefficient b: "))
+c = float(input("Enter the coefficient c: "))
+# to calculate discriminant
+discriminant = b**2 - 4*a*c
+# to sheck if roots are real
+if discriminant >= 0:
+    root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    print(f"The roots are: {root1} and {root2}")
+else:
+    print("No real roots exist.")
