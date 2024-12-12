d33ae42b53836295bb790555891ae1624ae88471
Yashaswini S <156496246+yasH04-s@users.noreply.github.com>
2024-12-12 20:13:53 +0530

    Update code.py

--- Experiment_1/V1_hardCodingVariables/code.py
+++ Experiment_1/V1_hardCodingVariables/code.py
@@ -1,1 +1,15 @@
+import math
+# hard code coefficients
+a = 1
+b = -3
+c = 2
+# to calculate discriminant
+discriminant = b**2 - 4*a*c
+# to check the roots
+if discriminant >= 0:
+    root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    print(f"The roots are: {root1} and {root2}")
+else:
+    print("No real roots exist.")
 
